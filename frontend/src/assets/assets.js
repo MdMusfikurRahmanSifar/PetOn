6d23138003e4df6294ac73b395ccb5d6eba518fa
MusfiqueUsSalehin <musfiqueishti@gmail.com>
2025-12-15 04:41:07 +0600

    some assets added

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,17 @@
+import logo from './favicon.png';
+import husky from './lovely-pet-portrait-isolated.jpg'
+import cat from './adorable-cat-lifestyle.jpg'
+import rabbit from './full-shot-kid-playing-with-dog.jpg'
+import dog from './38708534_8612017.png'
+import group from './group-cute-animals-isolated-white-background.png'
+
+
+
+export const assets = {
+  logo,
+  husky,   
+  cat,
+  rabbit,
+  dog,
+  group
+}
